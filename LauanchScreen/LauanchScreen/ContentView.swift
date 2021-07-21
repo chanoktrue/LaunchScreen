6af56f412741332221203a55b4f3efbb8cc4ec0c
chanoktrue <chanoktrue@me.com>
2021-07-21 15:01:42 +0700

    up to github

---
+++ LauanchScreen/LauanchScreen/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LauanchScreen
+//
+//  Created by Thongchai Subsaidee on 21/7/2564 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LauanchScreen/LauanchScreen/LauanchScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  LauanchScreenApp.swift
+//  LauanchScreen
+//
+//  Created by Thongchai Subsaidee on 21/7/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct LauanchScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
